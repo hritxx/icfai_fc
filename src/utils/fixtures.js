d6523e3363d63e861954c664d27a8441ac4870e9
hritxx <hriteekroy1869@gmail.com>
2024-07-25 17:10:49 +0530

    update src

--- src/utils/fixtures.js
+++ src/utils/fixtures.js
@@ -2,12 +2,12 @@ export const matchesData = [
   {
     team1: {
       name: "Team A",
-      logo: "/logo.png",
+      logo: "public/logo.png",
       score: 2,
     },
     team2: {
       name: "Team B",
-      logo: "/chelsea.png",
+      logo: "public/chelsea.png",
       score: 1,
     },
     date: "July 9, 2024",
@@ -16,12 +16,12 @@ export const matchesData = [
   {
     team1: {
       name: "Team C",
-      logo: "/chelsea.png",
+      logo: "public/chelsea.png",
       score: 3,
     },
     team2: {
       name: "Team D",
-      logo: "/ManUtd.png",
+      logo: "public/logo.png",
       score: 2,
     },
     date: "July 10, 2024",
@@ -30,12 +30,12 @@ export const matchesData = [
   {
     team1: {
       name: "Team E",
-      logo: "https://via.placeholder.com/100",
+      logo: "public/logo.png",
       score: 0,
     },
     team2: {
       name: "Team F",
-      logo: "https://via.placeholder.com/100",
+      logo: "public/logo.png",
       score: 4,
     },
     date: "July 11, 2024",
@@ -44,12 +44,12 @@ export const matchesData = [
   {
     team1: {
       name: "Team A",
-      logo: "/logo.png",
+      logo: "public/logo.png",
       score: 2,
     },
     team2: {
       name: "Team B",
-      logo: "/logo.png",
+      logo: "public/logo.png",
       score: 1,
     },
     date: "July 9, 2024",
