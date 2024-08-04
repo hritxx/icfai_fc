9223267613df27b8f7fbd34a1c8ba289cd63a8ad
hritxx <hriteekroy1869@gmail.com>
2024-08-05 01:55:45 +0530

    image fixing

--- src/utils/slider.js
+++ src/utils/slider.js
@@ -3,49 +3,49 @@ export const players = [
     name: "Baichung Bhutia",
     price: "28 Years",
     detail: "Midfielder",
-    image: "/Players/mbappe.jpg", // Updated path
+    image: "/icfai_fc/Players/mbappe.jpg", // Updated path
     jerseyNumber: "10",
   },
   {
     name: "Lionel Messi",
     price: "28 Years",
     detail: "Left back",
-    image: "/Players/mbappe.jpg", // Updated path
+    image: "/icfai_fc/Players/mbappe.jpg", // Updated path
     jerseyNumber: "10",
   },
   {
     name: "Christiano Ronaldo",
     price: "28 Years",
     detail: "Right back",
-    image: "/Players/mbappe.jpg", // Updated path
+    image: "/icfai_fc/Players/mbappe.jpg", // Updated path
     jerseyNumber: "10",
   },
   {
     name: "Neymar Jr.",
     price: "28 Years",
     detail: "Right Winger",
-    image: "/Players/mbappe.jpg", // Updated path
+    image: "/icfai_fc/Players/mbappe.jpg", // Updated path
     jerseyNumber: "10",
   },
   {
     name: "Asatti Garden City",
     price: "66,353",
     detail: "Left Winger",
-    image: "/Players/mbappe.jpg", // Updated path
+    image: "/icfai_fc/Players/mbappe.jpg", // Updated path
     jerseyNumber: "10",
   },
   {
     name: "Citralan Puri Serang",
     price: "28 Years",
     detail: "Goalkeeper",
-    image: "/Players/mbappe.jpg", // Updated path
+    image: "/icfai_fc/Players/mbappe.jpg", // Updated path
     jerseyNumber: "10",
   },
   {
     name: "Antonio Rudiger",
     price: "28 Years",
     detail: "Left back",
-    image: "../Players/mbappe.jpg",
+    image: "/icfai_fc/Players/mbappe.jpg", // Updated path
     jerseyNumber: "10",
   },
   {
@@ -58,19 +58,19 @@ export const players = [
     name: "Kai Havertz",
     price: "28 Years",
     detail: "Right Winger",
-    image: "../Players/mbappe.jpg",
+    image: "/icfai_fc/Players/mbappe.jpg", // Updated path
   },
   {
     name: "Christian Pulisic",
     price: "28 Years",
     detail: "Left Winger",
-    image: "../Players/mbappe.jpg",
+    image: "/icfai_fc/Players/mbappe.jpg", // Updated path
   },
   {
     name: "Peter Cech",
     price: "28 Years",
     detail: "Goalkeeper",
-    image: "../Players/mbappe.jpg",
+    image: "/icfai_fc/Players/mbappe.jpg", // Updated path
   },
 ];
 
@@ -79,19 +79,19 @@ export const coaches = [
     name: "Baichung Bhutia",
     price: "28 Years",
     detail: "Midfielder",
-    image: "/Players/mbappe.jpg", // Updated path
+    image: "/icfai_fc/Players/mbappe.jpg", // Updated path
   },
   {
     name: "Lionel Messi",
     price: "28 Years",
     detail: "Left back",
-    image: "/Players/mbappe.jpg", // Updated path
+    image: "/icfai_fc/Players/mbappe.jpg", // Updated path
   },
   {
     name: "Christiano Ronaldo",
     price: "28 Years3",
     detail: "Right back",
-    image: "/Players/mbappe.jpg", // Updated path
+    image: "/icfai_fc/Players/mbappe.jpg", // Updated path
   },
 ];
 1920;
